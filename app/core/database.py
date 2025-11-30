1a98b4aab2915e7ea57fc800172669c5294d1147
Lev <levmihel150@gmail.com>
2025-11-30 12:51:17 +0700

    Create get_session function

---
+++ app/core/database.py
@@ -0,0 +1,25 @@
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
+
+from settings import settings
+
+
+engine = create_async_engine(
+    settings.DATABASE_URL,
+    future=True,
+    echo=False,
+)
+
+session_factory = async_sessionmaker(
+    bind=engine,
+    class_=AsyncSession,
+    expire_on_commit=False,
+    autoflush=False,
+)
+
+
+async def get_session():
+    async with session_factory() as session:
+        try:
+            yield session
+        finally:
+            await session.close()
