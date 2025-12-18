a0e8f631a6769bd60cc3816aab053790c7eac21d
Lev <levmihel150@gmail.com>
2025-12-18 20:04:21 +0700

    Update test record response

--- app/schemas/Record.py
+++ app/schemas/Record.py
@@ -20,8 +20,9 @@ class RecordCreate(BaseModel):
     @field_validator("price")
     @classmethod
     def validate_price(cls, v):
-        if v < 0:
-            raise ValueError("Price must be greater than 0")
+        if v is not None:
+            if v < 0:
+                raise ValueError("Price must be greater or equal than 0")
         return v
 
     @field_validator("status")
@@ -43,9 +44,9 @@ class RecordResponse(BaseModel):
     id: Annotated[int, Field(..., description="ID of record")]
     date: Annotated[date, Field(..., description="Date of record", examples=[date.today()])]
     time: Annotated[time, Field(..., description="Time of record", examples=[time(14, 30)])]
-    status: Annotated[str, Field(None, max_length=30, description="Status of record", examples=ALLOWED_STATUSES)]
-    price: Annotated[float, Field(None, description="Price of record", examples=[500, 1500, 1410.10])]
-    notes: Annotated[str, Field(
+    status: Annotated[str, Field(..., max_length=30, description="Status of record", examples=ALLOWED_STATUSES)]
+    price: Annotated[float, Field(..., description="Price of record", examples=[500, 1500, 1410.10])]
+    notes: Annotated[Optional[str], Field(
         None,
         description="Notes of record",
         examples=["Customer requested morning appointment", "Special requirements"]
@@ -55,8 +56,9 @@ class RecordResponse(BaseModel):
     @field_validator("price")
     @classmethod
     def validate_price(cls, v):
-        if v < 0:
-            raise ValueError("Price must be greater than 0")
+        if v is not None:
+            if v < 0:
+                raise ValueError("Price must be greater or equal than 0")
         return v
 
     @field_validator("status")
@@ -64,13 +66,6 @@ class RecordResponse(BaseModel):
     def validate_status(cls, v):
         if v not in ALLOWED_STATUSES:
             raise ValueError(f"Status must be one of {ALLOWED_STATUSES}")
-        return v
-
-    @field_validator("date")
-    @classmethod
-    def validate_date(cls, v):
-        if v < date.today():
-            raise ValueError("Date must be in the future")
         return v
 
     model_config = ConfigDict(from_attributes=True)
@@ -90,8 +85,9 @@ class RecordUpdate(BaseModel):
     @field_validator("price")
     @classmethod
     def validate_price(cls, v):
-        if v is not None and v <= 0:
-            raise ValueError("Price must be greater than 0")
+        if v is not None:
+            if v < 0:
+                raise ValueError("Price must be greater or equal than 0")
         return v
 
     @field_validator("status")
@@ -118,6 +114,8 @@ class EditRecordStatus(BaseModel):
     def validate_status(cls, v):
         if v is not None and v not in ALLOWED_STATUSES:
             raise ValueError(f"Status must be one of {ALLOWED_STATUSES}")
+        elif v is None:
+            raise ValueError("Status is None")
         return v
 
 
--- tests/unit/test_schemas/test_record.py
+++ tests/unit/test_schemas/test_record.py
@@ -1,8 +1,9 @@
-from datetime import date, time
+from datetime import date, time, timedelta
 
 import pytest
 from pydantic import ValidationError
 
+from app.models import Record
 from app.schemas.Record import (
 RecordResponse,
 RecordCreate,
@@ -31,7 +32,7 @@ class TestRecordCreate:
         assert record.notes == data["notes"]
         assert record.user_id == data["user_id"]
 
-    def test_record_with_only_not_optional_fields(self):
+    def test_record_with_only_non_optional_fields(self):
         data = {
             "date": date.today(),
             "time": time(14, 30),
@@ -45,9 +46,24 @@ class TestRecordCreate:
         assert record.status is None
         assert record.notes is None
 
+    def test_record_with_only_none_in_price(self):
+        data = {
+            "date": date.today(),
+            "time": time(14, 30),
+            "user_id": 1,
+            "price": None,
+        }
+        record = RecordCreate(**data)
+        assert record.date == data["date"]
+        assert record.time == data["time"]
+        assert record.user_id == data["user_id"]
+        assert record.price is None
+        assert record.status is None
+        assert record.notes is None
+
     def test_record_with_wrong_date(self):
         data = {
-            "date": date(2024, 12, 31),
+            "date": date.today() - timedelta(1),
             "time": time(14, 30),
             "user_id": 1
         }
@@ -66,7 +82,7 @@ class TestRecordCreate:
 
     def test_record_without_time(self):
         data = {
-            "date": date(2024, 12, 31),
+            "date": date.today(),
             "user_id": 1
         }
         with pytest.raises(ValidationError) as exc_info:
@@ -75,7 +91,7 @@ class TestRecordCreate:
 
     def test_record_without_user_id(self):
         data = {
-            "date": date(2024, 12, 31),
+            "date": date.today(),
             "time": time(14, 30),
         }
         with pytest.raises(ValidationError) as exc_info:
@@ -122,7 +138,22 @@ class TestRecordCreate:
             record = RecordCreate(**data)
         assert len(exc_info.value.errors()) == 1
         assert exc_info.value.errors()[0]["type"] == "value_error"
-        assert "Price must be greater than 0" in str(exc_info.value)
+        assert "Price must be greater or equal than 0" in str(exc_info.value)
+
+    def test_record_with_zero_price_should_pass(self):
+        data = {
+            "date": date.today(),
+            "time": time(14, 30),
+            "user_id": 1,
+            "price": 0.0,
+        }
+        record = RecordCreate(**data)
+        assert record.date == data["date"]
+        assert record.time == data["time"]
+        assert record.status is None
+        assert record.user_id == data["user_id"]
+        assert record.price == data["price"]
+        assert record.notes is None
 
 class TestRecordResponse:
     def test_record_response(self):
@@ -136,16 +167,16 @@ class TestRecordResponse:
             "user_id": 1,
         }
         record = RecordResponse(**data)
+
         assert record.id == data["id"]
         assert record.date == data["date"]
         assert record.time == data["time"]
         assert record.status == data["status"]
         assert record.notes == data["notes"]
-        assert record.user_id == data["user_id"]
         assert record.price == data["price"]
-        assert record.notes == data["notes"]
+        assert record.user_id == data["user_id"]
 
-    def test_record_response(self):
+    def test_record_response_from_model(self):
         data = {
             "id": 1,
             "date": date.today(),
@@ -155,12 +186,163 @@ class TestRecordResponse:
             "price": 500,
             "user_id": 1,
         }
-        record = RecordResponse(**data)
+        data_model = Record(**data)
+        record = RecordResponse.model_validate(data_model)
+
         assert record.id == data["id"]
         assert record.date == data["date"]
         assert record.time == data["time"]
         assert record.status == data["status"]
         assert record.notes == data["notes"]
-        assert record.user_id == data["user_id"]
         assert record.price == data["price"]
+        assert record.user_id == data["user_id"]
+
+    def test_record_response_from_model_with_empty_notes(self):
+        data = {
+            "id": 1,
+            "date": date.today(),
+            "time": time(14, 30),
+            "status": "created",
+            "price": 500,
+            "user_id": 1,
+        }
+        data_model = Record(**data)
+        record = RecordResponse.model_validate(data_model)
+
+        assert record.id == data["id"]
+        assert record.date == data["date"]
+        assert record.time == data["time"]
+        assert record.status == data["status"]
+        assert record.notes is None
+        assert record.price == data["price"]
+        assert record.user_id == data["user_id"]
+
+    def test_record_response_from_model_with_none_in_notes(self):
+        data = {
+            "id": 1,
+            "date": date.today(),
+            "time": time(14, 30),
+            "status": "created",
+            "price": 500,
+            "notes": None,
+            "user_id": 1,
+        }
+        data_model = Record(**data)
+        record = RecordResponse.model_validate(data_model)
+
+        assert record.id == data["id"]
+        assert record.date == data["date"]
+        assert record.time == data["time"]
+        assert record.status == data["status"]
         assert record.notes == data["notes"]
+        assert record.price == data["price"]
+        assert record.user_id == data["user_id"]
+
+    def test_record_response_from_model_with_wrong_status(self):
+        data = {
+            "id": 1,
+            "date": date.today(),
+            "time": time(14, 30),
+            "status": "qwe",
+            "notes": "Notes",
+            "price": 500,
+            "user_id": 1,
+        }
+        data_model = Record(**data)
+        with pytest.raises(ValidationError) as exc_info:
+            record = RecordResponse.model_validate(data_model)
+
+        assert len(exc_info.value.errors()) == 1
+        assert exc_info.value.errors()[0]["type"] == "value_error"
+        assert "Status must be one of" in str(exc_info.value)
+
+    def test_record_response_from_model_with_wrong_price(self):
+        data = {
+            "id": 1,
+            "date": date.today(),
+            "time": time(14, 30),
+            "status": "created",
+            "notes": "Notes",
+            "price": -50,
+            "user_id": 1,
+        }
+        data_model = Record(**data)
+        with pytest.raises(ValidationError) as exc_info:
+            record = RecordResponse.model_validate(data_model)
+
+        assert len(exc_info.value.errors()) == 1
+        assert exc_info.value.errors()[0]["type"] == "value_error"
+        assert "Price must be greater or equal than 0" in str(exc_info.value)
+
+    def test_record_response_from_model_with_past_date_should_pass(self):
+        data = {
+            "id": 1,
+            "date": date.today() - timedelta(days=1),
+            "time": time(14, 30),
+            "status": "created",
+            "notes": "Notes",
+            "price": 500,
+            "user_id": 1,
+        }
+        data_model = Record(**data)
+        record = RecordResponse.model_validate(data_model)
+
+        assert record.id == data["id"]
+        assert record.date == data["date"]
+        assert record.time == data["time"]
+        assert record.status == data["status"]
+        assert record.notes == data["notes"]
+        assert record.price == data["price"]
+        assert record.user_id == data["user_id"]
+
+    def test_record_response_with_zero_price(self):
+        data = {
+            "id": 1,
+            "date": date.today(),
+            "time": time(14, 30),
+            "status": "created",
+            "notes": "Notes",
+            "price": 0,
+            "user_id": 1,
+        }
+        record = RecordResponse(**data)
+
+        assert record.id == data["id"]
+        assert record.date == data["date"]
+        assert record.time == data["time"]
+        assert record.status == data["status"]
+        assert record.notes == data["notes"]
+        assert record.price == data["price"]
+        assert record.user_id == data["user_id"]
+
+    def test_record_response_tomorrow(self):
+        data = {
+            "id": 1,
+            "date": date.today() + timedelta(days=1),
+            "time": time(14, 30),
+            "status": "created",
+            "notes": "Notes",
+            "price": 500,
+            "user_id": 1,
+        }
+        record = RecordResponse(**data)
+
+        assert record.id == data["id"]
+        assert record.date == data["date"]
+        assert record.time == data["time"]
+        assert record.status == data["status"]
+        assert record.notes == data["notes"]
+        assert record.price == data["price"]
+        assert record.user_id == data["user_id"]
+
+
+class TestRecordUpdate:
+    pass
+
+
+class TestEditRecordStatus:
+    pass
+
+
+class TestEditRecordNote:
+    pass
