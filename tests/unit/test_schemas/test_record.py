279c3c079e5c3e8041df8f4071544f80eace0eb7
Lev <levmihel150@gmail.com>
2025-12-18 20:38:12 +0700

    Create tests for edit record status

--- tests/unit/test_schemas/test_record.py
+++ tests/unit/test_schemas/test_record.py
@@ -155,6 +155,7 @@ class TestRecordCreate:
         assert record.price == data["price"]
         assert record.notes is None
 
+
 class TestRecordResponse:
     def test_record_response(self):
         data = {
@@ -469,8 +470,54 @@ class TestRecordUpdate:
         assert exc_info.value.errors()[0]["type"] == "value_error"
         assert "Price must be greater or equal than 0" in str(exc_info.value)
 
+
 class TestEditRecordStatus:
-    pass
+    def test_edit_record_status_to_created(self):
+        data = {
+            "id": 1,
+            "status": "created",
+        }
+        record = EditRecordStatus(**data)
+        assert record.id == data["id"]
+        assert record.status == data["status"]
+
+    def test_edit_record_status_to_confirmed(self):
+        data = {
+            "id": 1,
+            "status": "confirmed",
+        }
+        record = EditRecordStatus(**data)
+        assert record.id == data["id"]
+        assert record.status == data["status"]
+
+    def test_edit_record_status_to_rejected(self):
+        data = {
+            "id": 1,
+            "status": "rejected",
+        }
+        record = EditRecordStatus(**data)
+        assert record.id == data["id"]
+        assert record.status == data["status"]
+
+    def test_edit_record_status_to_cancelled(self):
+        data = {
+            "id": 1,
+            "status": "cancelled",
+        }
+        record = EditRecordStatus(**data)
+        assert record.id == data["id"]
+        assert record.status == data["status"]
+
+    def test_edit_record_status_to_unconfirmed(self):
+        data = {
+            "id": 1,
+            "status": "qwe",
+        }
+        with pytest.raises(ValidationError) as exc_info:
+            record = EditRecordStatus(**data)
+        assert len(exc_info.value.errors()) == 1
+        assert exc_info.value.errors()[0]["type"] == "value_error"
+        assert "Status must be one of" in str(exc_info.value)
 
 
 class TestEditRecordNote:
