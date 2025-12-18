f45c785feca98517197ed975ecf6ca3b389f7f08
Lev <levmihel150@gmail.com>
2025-12-18 20:44:58 +0700

    Create test edit record note

--- tests/unit/test_schemas/test_record.py
+++ tests/unit/test_schemas/test_record.py
@@ -521,4 +521,39 @@ class TestEditRecordStatus:
 
 
 class TestEditRecordNote:
-    pass
+    def test_edit_record_note_short(self):
+        data = {
+            "id": 1,
+            "notes": "Note",
+        }
+        record = EditRecordNote(**data)
+        assert record.id == data["id"]
+        assert record.notes == data["notes"]
+
+    def test_edit_record_note_long(self):
+        data = {
+            "id": 1,
+            "notes": "asdsadasdasdasvdgavsgdvahgsdvasdasdasdsfdshuv dscs bduwhb nvjdkw vijwfen vijkw vefknfkhsvdya"
+                    "sfdvyasdvadqimvodfwnmv iofdmnv oim iowf mmfwvo emdv oikfwd niodk fvmosytdvasytdvasytdvasydtvasy"
+                    "tdvgasodkfn vwfn okfdl moin wodfn kmo,v miodn jkn doi kmo ifnm iew omf ovkdvbaygsdvgysvdygas",
+        }
+        record = EditRecordNote(**data)
+        assert record.id == data["id"]
+        assert record.notes == data["notes"]
+
+    def test_edit_record_note_empty(self):
+        data = {
+            "id": 1,
+            "notes": "",
+        }
+        record = EditRecordNote(**data)
+        assert record.id == data["id"]
+        assert record.notes == data["notes"]
+
+    def test_edit_record_note_is_none(self):
+        data = {
+            "id": 1,
+        }
+        record = EditRecordNote(**data)
+        assert record.id == data["id"]
+        assert record.notes is None
