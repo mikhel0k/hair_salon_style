586c9a8175f0ce2c3eab34bf7d3096b216b39c64
Lev <levmihel150@gmail.com>
2025-12-19 11:37:08 +0700

    Create tests for make record schema

--- tests/unit/test_schemas/record/test_record_create.py
+++ tests/unit/test_schemas/record/test_record_create.py
@@ -25,7 +25,7 @@ class TestRecordCreate:
         assert record.notes == data["notes"]
         assert record.user_id == data["user_id"]
 
-    def test_record_with_only_non_optional_fields(self):
+    def test_record_create_with_only_non_optional_fields(self):
         data = {
             "date": date.today(),
             "time": time(14, 30),
@@ -39,7 +39,7 @@ class TestRecordCreate:
         assert record.status is None
         assert record.notes is None
 
-    def test_record_with_only_none_in_price(self):
+    def test_record_create_with_only_none_in_price(self):
         data = {
             "date": date.today(),
             "time": time(14, 30),
@@ -54,7 +54,7 @@ class TestRecordCreate:
         assert record.status is None
         assert record.notes is None
 
-    def test_record_with_wrong_date(self):
+    def test_record_create_yesterday(self):
         data = {
             "date": date.today() - timedelta(1),
             "time": time(14, 30),
@@ -64,7 +64,7 @@ class TestRecordCreate:
             record = RecordCreate(**data)
         assert "Date must be in the future" in str(exc_info.value)
 
-    def test_record_without_date(self):
+    def test_record_create_without_date(self):
         data = {
             "time": time(14, 30),
             "user_id": 1
@@ -73,7 +73,7 @@ class TestRecordCreate:
             record = RecordCreate(**data)
         assert any(error["type"] == "missing" for error in exc_info.value.errors())
 
-    def test_record_without_time(self):
+    def test_record_create_without_time(self):
         data = {
             "date": date.today(),
             "user_id": 1
@@ -82,7 +82,7 @@ class TestRecordCreate:
             record = RecordCreate(**data)
         assert any(error["type"] == "missing" for error in exc_info.value.errors())
 
-    def test_record_without_user_id(self):
+    def test_record_create_without_user_id(self):
         data = {
             "date": date.today(),
             "time": time(14, 30),
@@ -91,7 +91,7 @@ class TestRecordCreate:
             record = RecordCreate(**data)
         assert any(error["type"] == "missing" for error in exc_info.value.errors())
 
-    def test_record_with_wrong_status(self):
+    def test_record_create_with_wrong_status(self):
         data = {
             "date": date.today(),
             "time": time(14, 30),
@@ -104,7 +104,7 @@ class TestRecordCreate:
         assert exc_info.value.errors()[0]["type"] == "value_error"
         assert "Status must be one of ['created', 'confirmed', 'rejected', 'cancelled']" in str(exc_info.value)
 
-    def test_record_with_all_statuses(self):
+    def test_record_create_with_all_statuses(self):
         for status in ALLOWED_STATUSES:
             data = {
                 "date": date.today(),
@@ -120,7 +120,7 @@ class TestRecordCreate:
             assert record.price is None
             assert record.notes is None
 
-    def test_record_with_negative_price(self):
+    def test_record_create_with_negative_price(self):
         data = {
             "date": date.today(),
             "time": time(14, 30),
@@ -133,7 +133,7 @@ class TestRecordCreate:
         assert exc_info.value.errors()[0]["type"] == "value_error"
         assert "Price must be greater or equal than 0" in str(exc_info.value)
 
-    def test_record_with_zero_price_should_pass(self):
+    def test_record_create_with_zero_price_should_pass(self):
         data = {
             "date": date.today(),
             "time": time(14, 30),
