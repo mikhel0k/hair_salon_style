e0500056c21bac5cfafe5bb076a9533bf0b30516
Lev <levmihel150@gmail.com>
2025-12-23 14:45:58 +0700

    Create schemas for master and service

---
+++ app/schemas/Master.py
@@ -0,0 +1,29 @@
+from typing import Annotated, Optional
+
+from pydantic import BaseModel, Field
+
+
+class MasterBase(BaseModel):
+    name: Annotated[str, Field(..., description="Name of the master")]
+    specialization: Annotated[str, Field(..., description="Specialization of the master")]
+    phone: Annotated[str, Field(..., description="Phone number of the master")]
+    email: Annotated[str, Field(..., description="Email address of the master")]
+    work_schedule: Annotated[str, Field(..., description="Work shedule of the master")]
+    status: Annotated[str, Field(..., description="Status of the master")]
+
+
+class MasterCreate(MasterBase):
+    pass
+
+
+class MasterResponse(MasterBase):
+    id: Annotated[int, Field(..., description="ID of the master")]
+
+
+class MasterUpdate(BaseModel):
+    name: Annotated[Optional[str], Field(..., description="Name of the master")]
+    specialization: Annotated[Optional[str], Field(..., description="Specialization of the master")]
+    phone: Annotated[Optional[str], Field(..., description="Phone number of the master")]
+    email: Annotated[Optional[str], Field(..., description="Email address of the master")]
+    work_schedule: Annotated[Optional[str], Field(..., description="Work shedule of the master")]
+    status: Annotated[Optional[str], Field(..., description="Status of the master")]
---
+++ app/schemas/Service.py
@@ -0,0 +1,27 @@
+from typing import Annotated, Optional
+
+from pydantic import BaseModel, Field
+
+
+class Service(BaseModel):
+    name: Annotated[str, Field(..., description="Name of the service")]
+    price: Annotated[float, Field(..., description="Price of the service")]
+    duration: Annotated[int, Field(..., description="Duration of the service")]
+    category: Annotated[str, Field(..., description="Category of the service")]
+    description: Annotated[str, Field(..., description="Description of the service")]
+
+
+class ServiceCreate(Service):
+    pass
+
+
+class ServiceResponse(Service):
+    id: Annotated[int, Field(..., description="ID of the service")]
+
+
+class ServiceUpdate(BaseModel):
+    name: Annotated[Optional[str], Field(..., description="Name of the service")]
+    price: Annotated[Optional[float], Field(..., description="Price of the service")]
+    duration: Annotated[Optional[int], Field(..., description="Duration of the service")]
+    category: Annotated[Optional[str], Field(..., description="Category of the service")]
+    description: Annotated[Optional[str], Field(..., description="Description of the service")]
