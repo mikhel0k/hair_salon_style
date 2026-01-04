f06d0b60ce215df208d9e084d71a0905b566b581
Lev <levmihel150@gmail.com>
2026-01-04 22:35:21 +0700

    Delete old tests

--- migrations/versions/7ab1dbb8cc07_upgrade_lengths.py
+++ migrations/versions/7ab1dbb8cc07_upgrade_lengths.py
@@ -13,7 +13,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision: str = '7ab1dbb8cc07'
-down_revision: Union[str, Sequence[str], None] = '7ada36adcd13'
+down_revision: Union[str, Sequence[str], None] = 'cd412ad69bdc'
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
