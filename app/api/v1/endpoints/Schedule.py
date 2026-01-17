90f2722859d33e7b47f30966531b9eb828603f5f
lev <levmihel150@gmail.com>
2026-01-17 13:05:24 +0700

    Make getting schedule for master by jwt token

--- app/api/v1/endpoints/Schedule.py
+++ app/api/v1/endpoints/Schedule.py
@@ -2,24 +2,25 @@ from fastapi import APIRouter, Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.core import get_session
-from app.schemas.Schedule import ScheduleResponse, ScheduleUpdate
+from app.schemas.Schedule import ScheduleUpdate
 from app.services import ScheduleService
+from app.core.dependencies import is_user_master
 
 router = APIRouter()
 
 
-@router.get("/master_id/{master_id}")
+@router.get("/")
 async def get_schedule_by_master_id(
-        master_id: int,
+        master=Depends(is_user_master),
         session: AsyncSession = Depends(get_session)
 ):
-    return await ScheduleService.get_schedule_by_master_id(master_id=master_id, session=session)
+    return await ScheduleService.get_schedule_by_master_id(master_id=master["sub"], session=session)
 
 
-@router.patch("/{schedule_id}")
+@router.patch("/")
 async def update_schedule(
-        schedule_id: int,
         schedule: ScheduleUpdate,
+        master=Depends(is_user_master),
         session: AsyncSession = Depends(get_session)
 ):
-    return await ScheduleService.update_schedule(schedule_id=schedule_id, schedule_data=schedule, session=session)
+    return await ScheduleService.update_schedule(schedule_id=master["sub"], schedule_data=schedule, session=session)
