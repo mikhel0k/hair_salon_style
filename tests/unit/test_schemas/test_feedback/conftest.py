3c49f7508664b51d1545cba6d9c81521d298bfa7
lev <levmihel150@gmail.com>
2026-01-18 20:30:06 +0700

    Create conftest for feedback schemas testing

---
+++ tests/unit/test_schemas/test_feedback/conftest.py
@@ -0,0 +1,23 @@
+class Estimation:
+    right_estimation_one = 1
+    right_estimation_two = 2
+    right_estimation_three = 3
+    right_estimation_four = 4
+    right_estimation_five = 5
+    wrong_estimation_six = 6
+    wrong_estimation_zero = 0
+    wrong_estimation_negative_one = -1
+    wrong_estimation_float = 1.5
+    wrong_estimation_false = False
+    wrong_estimation_true = True
+    wrong_estimation_none = None
+    wrong_estimation_string = "string"
+
+
+class Comment:
+    right_short_comment = "bla bla bla"
+    right_long_comment = "bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla"
+    right_int_comment = 1
+    right_float_comment = 1.5
+    right_bool_comment = False
+    right_none_comment = None
